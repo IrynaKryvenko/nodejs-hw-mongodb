3bd24e1ddb6455245302dbb27860115f5ed21faa
IrynaKryvenko <kryvenkoirina93@gmail.com>
2024-12-18 14:57:05 +0200

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,7 +2,9 @@ import { ContactsCollection } from "../db/models/contact.js";
 import { calculatePaginationData } from "../utils/calculatePaginationData.js";
 import { SORT_ORDER } from "../constants/index.js";
 
-export const getAllContacts = async ({  page = 1,
+
+export const getAllContacts = async ({
+  page = 1,
   perPage = 10,
   sortOrder = SORT_ORDER.ASC,
   sortBy = '_id',
@@ -12,8 +14,8 @@ export const getAllContacts = async ({  page = 1,
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = ContactsCollection.find();
-    const contactsCount = await ContactsCollection.find().merge(contactsQuery).countDocuments();
+  const contactsQuery = ContactsCollection.find({ userId });
+    const contactsCount = await ContactsCollection.find({ userId }).merge(contactsQuery).countDocuments();
   const contacts = await contactsQuery.skip(skip).limit(limit).sort({ [sortBy]: sortOrder }).exec();
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
     return {
