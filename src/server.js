7e28c85f50c3719ac00396ed146099612cb24fd5
IrynaKryvenko <kryvenkoirina93@gmail.com>
2025-01-04 20:04:16 +0200

    +

--- src/server.js
+++ src/server.js
@@ -1,42 +1,52 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { env } from './utils/env.js';
-import router from './routers/index.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
-import { sendEmailError } from './middlewares/sendEmailError.js';
+
+import router from './routers/index.js';
+
+import { env } from './utils/env.js';
+
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
-  const app = express();
+    const app = express();
 
-  app.use(express.json());
-  app.use(cors());
-  app.use(cookieParser());
+    app.use(express.json({
+        type: ['application/json', 'application/vnd.api+json'],
+    }));
 
-  app.use('/api-docs', swaggerDocs());
+    app.use(cors());
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+    app.use(cookieParser());
 
-  app.use(router);
+    app.use(
+        pino({
+            transport: {
+                target: 'pino-pretty',
+            },
+        }),
+    );
 
-  app.use('*', notFoundHandler);
+    app.use(router);
 
-  app.use('/auth/send-reset-email', sendEmailError);
+    app.get('/', (req, res) => {
+        res.json({ message: 'Server is enable' });
+    });
 
-  app.use(errorHandler);
+    app.use('*', notFoundHandler);
+    app.use(errorHandler);
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+
+    app.use('/api-docs', swaggerDocs());
+
 };
+
